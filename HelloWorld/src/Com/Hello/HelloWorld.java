9964e2c0b34c5f7dc9c6237c252ea6cc950e64f7
akanksha <aakankshaaj04@gmail.com>
2024-12-31 19:41:22 -0800

    Initial Commit.

---
+++ HelloWorld/src/Com/Hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package Com.Hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!!!");
+	}
+
+}
